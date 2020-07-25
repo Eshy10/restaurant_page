bc33832bf92a90dea0c951484c2c06b1ec6b8780
modupe <adewalebose15@gmail.com>
2020-07-25 21:25:39 +0100

    Add footer to dom

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { Menu } from './js/menu';
 import { sectionTestimonial } from './js/testimonial';
 import { Testimonial } from './js/testimonial';
 import { callAction } from './js/call';
+import { footer } from './js/footer';
 
 const content = document.querySelector('#content');
 content.appendChild(navbar);
@@ -13,6 +14,7 @@ content.appendChild(header);
 content.appendChild(menu);
 content.appendChild(sectionTestimonial);
 content.appendChild(callAction);
+content.appendChild(footer);
 
 const headerTime = (() => {
     let countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
---
+++ src/js/footer.js
@@ -0,0 +1,61 @@
+export const footer = (() => {
+const footer = document.createElement('footer');
+footer.setAttribute('class', 'footer bg-light')
+const footerContainer = document.createElement('div');
+footerContainer.setAttribute('class', 'container');
+const rowFooter = document.createElement('div');
+rowFooter.setAttribute('class', 'row');
+const colList = document.createElement('div');
+colList.setAttribute('class', 'col-lg-6 h-100 text-center text-lg-left my-auto');
+const socialList = document.createElement('ul');
+socialList.setAttribute('class', 'list-inline mb-2');
+socialList.innerHTML = `
+<li class="list-inline-item">
+<a href="#">About</a>
+</li>
+<li class="list-inline-item">&sdot;</li>
+<li class="list-inline-item">
+<a href="#">Contact</a>
+</li>
+<li class="list-inline-item">&sdot;</li>
+<li class="list-inline-item">
+<a href="#">Terms of Use</a>
+</li>
+<li class="list-inline-item">&sdot;</li>
+<li class="list-inline-item">
+<a href="#">Privacy Policy</a>
+</li>
+`
+const footerText = document.createElement('p');
+footerText.setAttribute('class', 'text-muted small mb-4 mb-lg-0');
+footerText.innerHTML = `&copy; Your Website 2020. All Rights Reserved.`;
+colList.appendChild(socialList);
+colList.appendChild(footerText);
+const colNetwrk = document.createElement('div');
+colNetwrk.setAttribute('class', 'col-lg-6 h-100 text-center text-lg-right my-auto');
+const networkList = document.createElement('ul');
+networkList.setAttribute('class', 'list-inline mb-0');
+networkList.innerHTML = `
+<li class="list-inline-item mr-3">
+<a href="#">
+  <i class="fab fa-facebook fa-2x fa-fw"></i>
+</a>
+</li>
+<li class="list-inline-item mr-3">
+<a href="#">
+  <i class="fab fa-twitter-square fa-2x fa-fw"></i>
+</a>
+</li>
+<li class="list-inline-item">
+<a href="#">
+  <i class="fab fa-instagram fa-2x fa-fw"></i>
+</a>
+</li>
+`
+colNetwrk.appendChild(networkList);
+rowFooter.appendChild(colList);
+rowFooter.appendChild(colNetwrk);
+footerContainer.appendChild(rowFooter);
+footer.appendChild(footerContainer);
+return footer;
+})();
